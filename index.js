695c0d037ab77ae69fb5eb35f2e42ce480c93667
fordefam12 <136336831+fordefam12@users.noreply.github.com>
2023-07-26 14:45:14 -0500

    show departments formated

--- index.js
+++ index.js
@@ -24,98 +24,84 @@ db.query("SELECT * FROM department", function (err, results) {
     console.log(err);
     return;
   }
-  console.table(results);
+  // console.table(results);
   userInput()
 });
 const userInput = () =>
   inquirer.prompt([
     {
-      type: "input",
-      name: "title",
-      message: "what is the title of the project?",
-    },
-
-    {
-      type: "input",
-      name: "description",
-      message: "what is the description of your project?",
-    },
-    
-    {
-      type: "input",
-      name: "installation",
-      message: "Describe the installation process if any:",
-    },
-    {
-      type: "input",
-      name: "usage",
-      message: "what is this project used for ?",
-    },
-    {
       type: "list",
-      name: "License",
-      message: "what licenses were used for the project?",
+      name: "options",
+      message: "what would you like to do ?",
       choices: [
-        "Academic Free License v3.0",
-        "Apache license 2.0",
-        "Artistic license 2.0",
-        "Boost Software License 1.0",
-        "BSD 2-clause license",
-        "BSD 3-clause license",
-        "BSD 3-clause Clear license",
-        "BSD Zero-Clause license",
-        "Creative Commons license family",
-        "Creative Commons Zero v1.0 Universal",
-        "Creative Commons Attribution 4.0",
-        "Creative Commons Attribution Share Alike 4.0",
-        "Do What The F*ck You Want To Public License",
-        "Educational Community License v2.0",
-        "Eclipse Public License 1.0",
-        "Eclipse Public License 2.0",
-        "European Union Public License 1.1",
-        "GNU Affero General Public License v3.0",
-        "GNU General Public License family",
-        "GNU General Public License v2.0",
-        "GNU General Public License v3.0",
-        "GNU Lesser General Public License family",
-        "GNU Lesser General Public License v2.1",
-        "GNU Lesser General Public License v3.0",
-        "ISC",
-        "LaTeX Project Public License v1.3c",
-        "Microsoft Public License",
-        "MIT",
-        "Mozilla Public License 2.0",
-        "Open Software License 3.0",
-        "PostgreSQL License",
-        "SIL Open Font License 1.1",
-        "University of Illinois/NCSA Open Source License",
-        "The Unlicense",
-        "zLib License",
+        "View all departments",
+        "View all roles",
+        "View all employees",
+        "Add a department",
+        "Add a role",
+        "Add an employee",
+        "update an employee role",
+        "Update an emploee Manager",
+        "View an employee by departments",
+        "delete an employee",
+        "delete a role",
+        "delete a department",
+        "view department budgets",
       ],
-    },
-    {
-      type: "input",
-      name: "Contributing",
-      message: "who all contributed on this project?",
-    },
-    {
-      type: "input",
-      name: "test",
-      message: "where any test included",
-    },
-    {
-      type: "input",
-      name: "questions",
-      message: "what do i do if i have a issue?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "what is your email ?",
-    },
-    {
-      type: "input",
-      name: "username",
-      message: "what is your github username?",
-    },
-  ]);
+    }
+  ])
+    .then((answers)=> {
+      const {choices} = answers
+    
+    
+      if (choices === "View all departments"){
+        showDepartments();
+      }
+      if (choices === "View all roles"){
+        showRoles();
+      }
+      if (choices === "View all employees"){
+        showEmployees();
+      }
+      if (choices === "Add a department"){
+        addDepartment();
+      }
+      if (choices === "Add a role"){
+        addRole();
+      }
+      if (choices === "Add an employee"){
+        addEmployee();
+      }
+      if (choices === "update an employee role"){
+        updateRole();
+      }
+      if (choices === "Update an emploee Manager"){
+        updateManager();
+      }
+      if (choices === "View an employee by departments"){
+        showEmployeeDepartments();
+      }
+      if (choices === "delete an employee"){
+        deleteEmployee();
+      }
+      if (choices === "delete a role"){
+        deleteRole();
+      }
+      if (choices === "delete a department"){
+        deleteDepartment();
+      }
+      if (choices === "view department budgets"){
+        showBugets();
+      }
+      
+  });
+  showDepartments = () => {
+      console.log("showing all departemnts.../n");
+      const sql = `SELECT department.id AS id, department  FROM department `;
+
+      db.promise().queery(sql, (err,rows) => {
+        if (err) throw err;
+        console.table(rows);
+        userInput();
+      })
+  }
